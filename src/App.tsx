02c37db5c28add2c30d4d87c8e777abdc8e92214
danpung2 <danpung_2@naver.com>
2023-02-15 16:00:22 +0900

    feat: get all chat room, create chat room

--- src/App.tsx
+++ src/App.tsx
@@ -1,64 +1,19 @@
-import React, {useEffect, useState} from 'react';
+import React from 'react';
 import './App.css';
-import {getAllRoomList, createRoom} from "./common/api/chat";
-import {forEach} from "react-bootstrap/ElementChildren";
+import {BrowserRouter, Route, Router, Routes, useLocation} from 'react-router-dom';
+import Main from "./page/Main";
+import {ROOT_PATH} from "./common/constants/path.const";
 
 
 function App() {
-
-    const roomList = getAllRoomList();
-
-    const [loading, setLoading] = useState(false);
-    const [roomName, setRoomName] = useState("");
-
-    const onChangeRoomName = (event: any) => {
-        setRoomName(event.target.value);
-    }
-
+    // const location = useLocation();
     return (
-        <>
-            <div className="App">
-                <div className="container" id="app">
-                    <div className="row">
-                        <div className="col-md-12">
-                            <h3>채팅방 리스트</h3>
-                        </div>
-                    </div>
-                    <div className="input-group">
-                        <div className="input-group-prepend">
-                            <label className="input-group-text">방제목</label>
-                        </div>
-                        <input type="text" className="form-control"/>
-                        <div className="input-group-append">
-                            <button className="btn btn-primary" type="submit" onClick={onChangeRoomName}>
-                                채팅방 개설
-                            </button>
-                        </div>
-                    </div>
-                </div>
-                <ul className="list-group">
-                    {roomList.then((res) => {
-                        res.forEach(function (){
-                            return (
-                                <li className="list-group-item list-group-item-action">
-                                    {roomName}
-                                </li>
-                            )
-                        })
-                    })}
-                    {/*{roomList.then((res) => {*/}
-                    {/*    */}
-                    {/*    res.map(room => {*/}
-                    {/*        return (*/}
-                    {/*            <li className="list-group-item list-group-item-action">*/}
-                    {/*                {room.roomName}*/}
-                    {/*            </li>*/}
-                    {/*        )*/}
-                    {/*    })*/}
-                    {/*})}*/}
-                </ul>
-            </div>
-        </>
+        <BrowserRouter>
+            <Routes>
+                <Route path={ROOT_PATH} element={<Main/>}/>
+            </Routes>
+        </BrowserRouter>
+
     )
 }
 
---
+++ src/common/constants/api.const.ts
@@ -0,0 +1,2 @@
+export const GET_ALL_CHAT_ROOM = "/api/chat/room/all";
+export const CREATE_ROOM = "/api/chat/room";
