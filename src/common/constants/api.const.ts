53eb5560e36d6fd9f031ef4ea7466152bfb2ea2e
danpung2 <danpung_2@naver.com>
2023-02-23 14:17:34 +0900

    fix

--- src/common/constants/api.const.ts
+++ src/common/constants/api.const.ts
@@ -1,5 +1,5 @@
 export const GET_ALL_CHAT_ROOM = "/api/chat/room/all";
 export const CREATE_ROOM = "/api/chat/room";
 export const ENTER_ROOM = "/api/chat/room/enter"
-export const LOGIN = "/api/user/login";
+export const LOGIN = "/login";
 export const JOIN = "/api/user";
